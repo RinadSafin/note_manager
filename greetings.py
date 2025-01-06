99dae2508649a0e5659fd45717b274428f8a9142
RinadSafin <rinadsafin@gmail.com>
2025-01-06 13:28:24 +0500

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -1,1 +1,14 @@
+from importlib.resources import contents
 
+username="Ринад"
+title="заметки"
+contents=("Описание")
+statistics="статус"
+cteated_date="06.01.2025"
+issue_date="01.10.2025"
+print("Имя пользователя:", username)
+print("Название:", title)
+print("Описание:", contents)
+print("Статус:", statistics)
+print("Дата начала:", cteated_date)
+print("Дата окончания:", issue_date)
