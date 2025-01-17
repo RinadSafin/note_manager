230b6d9219a43503b3e648d13b98ef4a7a201407
RinadSafin <rinadsafin@gmail.com>
2025-01-17 20:27:51 +0500

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,2 +1,9 @@
-temp_created_date="06.01.25,06/01/25"
-temp_issue_date="01.10.25,01/10/25"
+date="01 января 2025"
+created_date="06.01.2025"
+issue_date="01.10.2025"
+temp_date=date[:10]
+temp_created_date=created_date[:5]
+temp_issue_date=issue_date[:5]
+print(temp_date)
+print(temp_created_date)
+print(temp_issue_date)
